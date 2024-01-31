2bf44fc7cc5ece49e55e518acf86c7841c922539
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2024-01-31 20:38:14 +0100

    Add files via upload

---
+++ WindowsFormsApp_Kitallo/WindowsFormsApp_Kitallo/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApp_Kitallo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTip = new System.Windows.Forms.TextBox();
+            this.buttonTip = new System.Windows.Forms.Button();
+            this.listBoxTip = new System.Windows.Forms.ListBox();
+            this.textBoxkitalalo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(311, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Találd ki a szót!";
+            // 
+            // textBoxTip
+            // 
+            this.textBoxTip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxTip.Location = new System.Drawing.Point(143, 195);
+            this.textBoxTip.Name = "textBoxTip";
+            this.textBoxTip.Size = new System.Drawing.Size(129, 35);
+            this.textBoxTip.TabIndex = 1;
+            this.textBoxTip.TextChanged += new System.EventHandler(this.textBoxTip_TextChanged);
+            // 
+            // buttonTip
+            // 
+            this.buttonTip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonTip.Location = new System.Drawing.Point(328, 195);
+            this.buttonTip.Name = "buttonTip";
+            this.buttonTip.Size = new System.Drawing.Size(142, 33);
+            this.buttonTip.TabIndex = 2;
+            this.buttonTip.Text = "TIP";
+            this.buttonTip.UseVisualStyleBackColor = true;
+            this.buttonTip.Click += new System.EventHandler(this.buttonTip_Click);
+            // 
+            // listBoxTip
+            // 
+            this.listBoxTip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBoxTip.FormattingEnabled = true;
+            this.listBoxTip.ItemHeight = 29;
+            this.listBoxTip.Location = new System.Drawing.Point(514, 146);
+            this.listBoxTip.Name = "listBoxTip";
+            this.listBoxTip.Size = new System.Drawing.Size(399, 207);
+            this.listBoxTip.TabIndex = 3;
+            // 
+            // textBoxkitalalo
+            // 
+            this.textBoxkitalalo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxkitalalo.Location = new System.Drawing.Point(143, 96);
+            this.textBoxkitalalo.Name = "textBoxkitalalo";
+            this.textBoxkitalalo.ReadOnly = true;
+            this.textBoxkitalalo.Size = new System.Drawing.Size(129, 35);
+            this.textBoxkitalalo.TabIndex = 4;
+            this.textBoxkitalalo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxkitalalo.TextChanged += new System.EventHandler(this.textBoxkitalalo_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 492);
+            this.Controls.Add(this.textBoxkitalalo);
+            this.Controls.Add(this.listBoxTip);
+            this.Controls.Add(this.buttonTip);
+            this.Controls.Add(this.textBoxTip);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTip;
+        private System.Windows.Forms.Button buttonTip;
+        private System.Windows.Forms.ListBox listBoxTip;
+        private System.Windows.Forms.TextBox textBoxkitalalo;
+    }
+}
+
---
+++ WindowsFormsApp_Kitallo/WindowsFormsApp_Kitallo/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp_Kitallo
+{
+    public partial class Form1 : Form
+    {
+        string[] data = File.ReadAllLines("szavak.txt");
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxkitalalo_TextChanged(object sender, EventArgs e)
+        {
+
+           int pos1 = rnd.Next(data.Length);
+           int pos2 = rnd.Next(data.Length);
+           int pos3 = rnd.Next(data.Length);
+           int pos4 = rnd.Next(data.Length);
+           
+           string word = data[];
+           
+           word = word.Remove(pos1, 1).Insert(pos1, ".");
+           word = word.Remove(pos2, 1).Insert(pos2, ".");
+           word = word.Remove(pos3, 1).Insert(pos3, ".");
+           word = word.Remove(pos3, 1).Insert(pos4, ".");
+
+        }
+
+        private void buttonTip_Click(object sender, EventArgs e)
+        {
+            listBoxTip.Items.Add($"{textBoxTip}");
+        }
+
+        private void textBoxTip_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxTip == textBoxkitalalo)
+            {
+                MessageBox.Show("Helyes megoldás");
+            }
+        }
+    }
+}
